ee81379966f2e2ed27b0c7121a4ad64257616ad5
Spyhunter2 <devdevstereo@gmail.com>
2026-03-21 14:57:07 -0700

    Added skeleton components and added standard color and font

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+//Import Tailwind CSS plugin for Vite
+import tailwindcss from "@tailwindcss/vite";
+//Gets the current file path and directory name for resolving paths
+import path from "path";
+//translates a file URL to a file path, which is necessary for using ES modules in Node.js to get the current file's path and directory.
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Add Tailwind CSS plugin to the Vite configuration
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
